a2a94293ce4181d57feb7c37bae42dccfa535321
Marcos Villalba <marcos.villalba@fortyau.com>
2025-01-05 18:11:54 -0300

    fix: open directly timer page if pwa is installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Timer from "./pages/Timer";
+import { useEffect, useState } from "react";
 
 function App() {
-  const isPWA = window.matchMedia("(display-mode: standalone)").matches;
+  const [isPWA, setIsPWA] = useState(false);
+
+  useEffect(() => {
+    setIsPWA(window.matchMedia("(display-mode: standalone)").matches);
+
+    const mediaQuery = window.matchMedia("(display-mode: standalone)");
+    const handleChange = (e: MediaQueryListEvent) => {
+      setIsPWA(e.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, []);
 
   return (
     <BrowserRouter>
@@ -11,8 +27,8 @@ function App() {
         <Route path="/" element={isPWA ? <Navigate to="/timer" /> : <Home />} />
         <Route
           path="/timer"
-          element={isPWA ? <Timer /> : <Navigate to="/" />}
-          //element={<Timer />}
+          //element={isPWA ? <Timer /> : <Navigate to="/" />}
+          element={<Timer />}
         />
       </Routes>
     </BrowserRouter>
